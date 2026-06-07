6a889ae862b9a0b5406269b3f67a95089cef2be3
Valentethedev <valente.artur123@gmail.com>
2026-06-07 18:16:06 -0300

    FEAT netinho

--- html/script.js
+++ html/script.js
@@ -12,4 +12,26 @@ fetch('dicionario.json')
         document.getElementById('link-malicioso').textContent = data.palavras[8]['Link Malicioso'];
         document.getElementById('otp').textContent = data.palavras[9]['OTP (One-Time Password)'];
     });
-    
+fetch('netinho.json')
+    .then(response => response.json())
+    .then(data => {
+
+        const container = document.getElementById('cards-container');
+
+        data.netinho.forEach(item => {
+
+            const card = document.createElement('div');
+            card.className = 'cards-container';
+
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">${item.pergunta}</h5>
+                    <p class="card-text">${item["o que fazer"]}</p>
+                </div>
+            `;
+
+            container.appendChild(card);
+        });
+
+    })
+    .catch(error => console.error('Erro ao carregar JSON:', error));
